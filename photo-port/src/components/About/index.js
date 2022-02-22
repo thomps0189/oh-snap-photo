687ab7f72b3acb19f2f068a9216f698267db05d6
thomps0189 <angelathompson2011@gmail.com>
2022-02-22 11:18:11 -0600

    nav and about sections

---
+++ photo-port/src/components/About/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import coverImage from "../../assets/cover/cover-image.jpg";
+function About() {
+  return (
+    <section className="my-5">
+      <h1 id="about">Who am I?</h1>
+      <img
+        src={coverImage}
+        className="my-2"
+        style={{ width: "100%" }}
+        alt="cover"
+      />
+      <div className="my-2">
+        <p>Lorem</p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
